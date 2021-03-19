26ef910ab3c85055f43980b7c6bbbdf2f2a7ce0e
sabouldahab-1988 <80941462+sabouldahab-1988@users.noreply.github.com>
2021-03-18 22:37:32 -0800

    Added Swagger
    -Added Swagger
    -Disabled SSL

---
+++ EmployeeAPI/EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+using DataAccessLayer.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EmployeeAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        EmployeeServices employeeService = new EmployeeServices();
+        [HttpGet]
+        [Route("EmployeeController/getEmployees")]
+        public IActionResult GetEmployees()
+        {
+            try
+            {
+                List<Employee> employees= employeeService.GetEmployees();
+                return new ObjectResult(employees)
+                {
+                    StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status200OK
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(ex)
+                {
+                    StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+    }
+}
--- EmployeeAPI/EmployeeAPI/Startup.cs
+++ EmployeeAPI/EmployeeAPI/Startup.cs
@@ -26,6 +26,8 @@ namespace EmployeeAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,16 @@ namespace EmployeeAPI
 
             app.UseHttpsRedirection();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample Demo");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +58,7 @@ namespace EmployeeAPI
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
